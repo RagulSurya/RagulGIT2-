a12ad1adeea8c286678eb6a6fb8e48efe86215ce
RagulSurya <ragulsurya77@gmail.com>
2019-09-19 10:50:27 +0530

    Commited the code - Changed

--- src/org/sample/test/KaapaaN.java
+++ src/org/sample/test/KaapaaN.java
@@ -5,8 +5,10 @@ public class KaapaaN {
 		String name = "SURYA";
 		String movie = "KAAPAAN";
 	    String date = "20-Sep-2019";
-	    System.out.println(name);
-	    System.out.println(movie);
-	    System.out.println(date);
+	    String date1 = "25-Sep-2019";
+	    System.out.println("Name of the Actor is " +name);
+	    System.out.println("Title of the Movie is "+movie);
+	    System.out.println("Actual Release Date of the Movie is "+date);
+	    System.out.println("Release Date of the Movie Changed To "+date1);
 }
 }
