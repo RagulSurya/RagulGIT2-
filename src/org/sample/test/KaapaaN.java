5cd41ed526bd13904346125d6c5dc1d9e2fcd302
RagulSurya <ragulsurya77@gmail.com>
2019-09-19 10:16:09 +0530

    I committed the code - done

---
+++ src/org/sample/test/KaapaaN.java
@@ -0,0 +1,12 @@
+package org.sample.test;
+
+public class KaapaaN {
+	public static void main(String[] args) {
+		String name = "SURYA";
+		String movie = "KAAPAAN";
+	    String date = "20-Sep-2019";
+	    System.out.println(name);
+	    System.out.println(movie);
+	    System.out.println(date);
+}
+}
